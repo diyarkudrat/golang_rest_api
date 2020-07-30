a56e3b2d92ae8ee9afdcaec9dcda7d79337ce32a
Diyar Kudrat <dkudrat@gmail.com>
2020-07-30 11:21:16 -0700

    data storage setup

--- server.go
+++ server.go
@@ -2,12 +2,39 @@ package main
 
 import "net/http"
 
-func playersHandler(w http.ResponseWriter, r *http.Request) {
+type Player struct {
+	FirstName string `json: "First Name"`
+	LastName  string `json: "Last Name"`
+	Height    string `json: "Height"`
+	Weight    int    `json: "Weight"`
+	State     string `json: "State"`
+}
+
+type playerHandlers struct {
+	store map[string]Player
+}
+
+func (p *playerHandlers) get(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func newPlayerHandlers() *playerHandlers {
+	return &playerHandlers{
+		store: map[string]Player{
+			"test": Player{
+				FirstName: "Diyar",
+				LastName:  "Kudrat",
+				Height:    "5'11",
+				Weight:    245,
+				State:     "CA",
+			},
+		},
+	}
+}
+
 func main() {
-	http.HandlerFunc("/", "")
+	playerHandlers := newPlayerHandlers()
+	http.HandleFunc("/players", playerHandlers.get)
 	err := http.ListenAndServe(":5000", nil)
 	if err != nil {
 		panic(err)
