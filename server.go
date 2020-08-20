62133583740ad560e2e7fcb64dcba66737fc0f2b
Diyar Kudrat <dkudrat@gmail.com>
2020-08-19 19:45:56 -0700

    added basic auth

--- server.go
+++ server.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 	"strings"
 	"sync"
 	"time"
@@ -132,10 +133,36 @@ func newPlayerHandlers() *playerHandlers {
 	}
 }
 
+type adminPortal struct {
+	password string
+}
+
+func newAdminPortal() *adminPortal {
+	password := os.Getenv("ADMIN_PASSWORD")
+	if password == "" {
+		panic("required ENV variable ADMIN_PASSWORD not set")
+	}
+
+	return &adminPortal{password: password}
+}
+
+func (a adminPortal) handler(w http.ResponseWriter, r *http.Request) {
+	user, pass, ok := r.BasicAuth()
+	if !ok || user != "admin" || pass != a.password {
+		w.WriteHeader(http.StatusUnauthorized)
+		w.Write([]byte("401 - unauthorized"))
+		return
+	}
+
+	w.Write([]byte("<html><h1>Secret admin portal</h1></html>"))
+}
+
 func main() {
+	admin := newAdminPortal()
 	playerHandlers := newPlayerHandlers()
 	http.HandleFunc("/players", playerHandlers.players)
 	http.HandleFunc("/players/", playerHandlers.getPlayer)
+	http.HandleFunc("/admin", admin.handler)
 	err := http.ListenAndServe(":5000", nil)
 	if err != nil {
 		panic(err)
