e2e835150b65e374760cf0f4c25b0f3def65c4a8
Diyar Kudrat <dkudrat@gmail.com>
2020-08-19 19:25:53 -0700

    GET request single player object through ID

--- server.go
+++ server.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strings"
 	"sync"
 	"time"
 )
@@ -65,6 +66,33 @@ func (p *playerHandlers) get(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func (p *playerHandlers) getPlayer(w http.ResponseWriter, r *http.Request) {
+	parts := strings.Split(r.URL.String(), "/")
+	if len(parts) != 3 {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	p.Lock()
+	player, ok := p.store[parts[2]]
+	p.Unlock()
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	jsonBytes, err := json.Marshal(player)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+	}
+
+	w.Header().Add("content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonBytes)
+
+}
+
 func (p *playerHandlers) post(w http.ResponseWriter, r *http.Request) {
 	// json data
 	bodyBytes, err := ioutil.ReadAll(r.Body)
@@ -93,29 +121,21 @@ func (p *playerHandlers) post(w http.ResponseWriter, r *http.Request) {
 	player.ID = fmt.Sprintf("%d", time.Now().UnixNano())
 
 	p.Lock()
-	p.store[player.FirstName] = player
+	p.store[player.ID] = player
 	defer p.Unlock()
 
 }
 
 func newPlayerHandlers() *playerHandlers {
 	return &playerHandlers{
-		store: map[string]Player{
-			"test": Player{
-				FirstName: "Diyar",
-				LastName:  "Kudrat",
-				Position:  "OG",
-				Height:    "5'11",
-				Weight:    245,
-				State:     "CA",
-			},
-		},
+		store: map[string]Player{},
 	}
 }
 
 func main() {
 	playerHandlers := newPlayerHandlers()
 	http.HandleFunc("/players", playerHandlers.players)
+	http.HandleFunc("/players/", playerHandlers.getPlayer)
 	err := http.ListenAndServe(":5000", nil)
 	if err != nil {
 		panic(err)
